789d960900128ce828f79b72507f9e147eeda626
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-02-08 03:02:41 +0530

    working with bootstrap components

--- Todo_list_version_01/src/App.jsx
+++ Todo_list_version_01/src/App.jsx
@@ -1,6 +1,34 @@
-function App(){
-  return <center class='todo-container'>
-    <h1>TODO APP</h1>
-  </center>
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+function App() {
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo />
+      
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">Buy milk</div>
+          <div className="col-4">6/06/2026</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6">Go to College</div>
+          <div className="col-4">6/06/2026</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 export default App;
